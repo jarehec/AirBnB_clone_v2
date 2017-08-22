ebd9358788507597f79472ce68c59acd6dc5f70a
jarehec <bone1198@gmail.com>
2017-08-22 02:18:06 +0000

    initial commit

---
+++ 1-pack_web_static.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from fabric.api import run
+import datetime
+"""generates a .tgz archive from the contents of the web_static folder
+"""
+
+
+def do_pack():
+    date = str(datetime.datetime.now().strftime("%Y%m%d%H%M%S"))
+    run("mkdir -p versions")
+    run("tar -cvzf versions/web_static_{}.tgz /data/web_static".format(date))
+    return ("versions/web_static_{}.tgz".format(date))
