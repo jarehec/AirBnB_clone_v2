c9afa36578e53a11b028da2f9f85c088b8e28118
jarehec <bone1198@gmail.com>
2017-08-26 05:44:05 +0000

    revert

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,9 +5,9 @@ apt install -y nginx
 mkdir -p /data/web_static/releases/test
 mkdir -p /data/web_static/shared
 echo 'Hello World!' > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/* /data/web_static/current
+ln -sf /data/web_static/releases/test /data/web_static/current
 chown -R ubuntu:ubuntu /data
 loc="location /hbnb_static {\n\talias /data/web_static/current;\n}"
-sed -i "41i $loc" /etc/nginx/sites-enabled/default
+sed -i "38i $loc" /etc/nginx/sites-available/default
 service nginx start
 service nginx reload
