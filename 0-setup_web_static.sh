935d98071af76949ff9db2449a873c29fc526094
jarehec <bone1198@gmail.com>
2017-08-26 05:30:53 +0000

    sites-available -> sites-enabled

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,6 +8,6 @@ echo 'Hello World!' > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/* /data/web_static/current
 chown -R ubuntu:ubuntu /data
 loc="location /hbnb_static {\n\talias /data/web_static/current;\n}"
-sed -i "38i $loc" /etc/nginx/sites-available/default
+sed -i "41i $loc" /etc/nginx/sites-enabled/default
 service nginx start
 service nginx reload
