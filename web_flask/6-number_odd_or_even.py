e598bb1b6ba86851422a8c06a516c612ef090428
jarehec <bone1198@gmail.com>
2017-09-06 20:01:21 +0000

    initial commit

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""
+"""
+
+from web_flask import app, render_template
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_isfun(text):
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_cool(text="is cool"):
+    return "Python {}".format(text.replace("_", " "))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number_route(n):
+    if type(n) is int:
+        return "{} is a number".format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    if type(n) is int:
+        return render_template('5-number.html', n=n)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def number_odd_or_even(n):
+    if type(n) is int:
+        if n % 2 == 0:
+            m = "even"
+        else:
+            m = "odd"
+        return render_template('6-number_odd_or_even.html', n=n, m=m)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
